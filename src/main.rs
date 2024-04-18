6ae6b025f45ff43f43c8dc10f9ab04bef153a20b
Diego Carnavalli Bolsoni <dibolsoni@gmail.com>
2024-04-18 17:36:22 -0300

    combat system

--- src/main.rs
+++ src/main.rs
@@ -62,8 +62,11 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.set_active_console(1);
+        self.resources.insert(Point::from_tuple(ctx.mouse_pos()));
         ctx.cls();
         ctx.set_active_console(2);
+        ctx.cls();
+        ctx.set_active_console(3);
         ctx.cls();
         self.resources.insert(ctx.key);
         let current_state = self.resources.get::<TurnState>().unwrap().clone();
@@ -83,8 +86,10 @@ fn main() {
         .with_tile_dimensions(32, 32)
         .with_resource_path("resources/")
         .with_font("dungeonfont.png", 32, 32)
+        .with_font("terminal8x8.png", 8, 8)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_sparse_console_no_bg(DISPLAY_WIDTH*4, DISPLAY_HEIGHT*4, "terminal8x8.png")
         .build()
         .unwrap();
 
